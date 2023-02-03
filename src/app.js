7b5000374dbc8ac2a0dbebe196f6d207308d61eb
Frank GP <fgp555@gmail.com>
2023-02-03 16:47:08 -0500

    finish

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 import expres from "express"
+import { pool } from "./db.js"
+import {PORT} from "./config.js"
 
 const app = expres()
 
-app.listen(3000)
-console.log("Server on http://localhost:3000")
+app.get("/", async (req, res) => {
+    const [rows] = await pool.query("SELECT * FROM users")
+    res.json(rows)
+})
+
+app.get("/ping", async (req, res) => {
+    const [result] = await pool.query('SELECT "hello world" as RESULT');
+    res.json(result[0])
+})
+
+app.get("/create", async (req, res) => {
+    const result = await pool.query("INSERT INTO users(name) VALUES('john')")
+    res.json(result)
+})
+
+app.listen(PORT)
+console.log("Server on http://localhost:3000", PORT)
